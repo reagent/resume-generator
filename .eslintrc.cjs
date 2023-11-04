47a2c34b0f046408124e347f0b6881204db0e3af
Patrick Reagan <2902+reagent@users.noreply.github.com>
2023-11-04 16:11:17 +0000

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+    ],
+    'no-duplicate-imports': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+  },
+};
